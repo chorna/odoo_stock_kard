e3863e32fcbececaa8360ecc5b57f803e8daff05
christian <christian1020@gmail.com>
2017-11-23 14:23:06 -0500

    models

---
+++ stock_kardex/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Stock Kardex',
+    'version': '1.0',
+    'category': 'Stock',
+    'description': """
+        Kardex del Producto en una Ubicación
+    """,
+    'depends': ['stock',],
+    'data': [
+        'views/menu.xml',
+        'views/kardex_lines.xml',
+        'wizard/stock_kardex_views.xml',
+    ],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+}
